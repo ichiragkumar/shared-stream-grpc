8a6032915e998856d44f0b14e630d72b2600e758
ichiragkumar <chiragroy3030@gmail.com>
2025-02-01 10:45:09 +0530

    added auth route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,39 +1,38 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import * as jwt from 'jsonwebtoken';
-import { Db } from 'mongodb';
-import { DatabaseService } from 'src/config/database.config';
+// import { Body, Controller, Post } from '@nestjs/common';
+// import { Db } from 'mongodb';
+// import { DatabaseService } from 'src/config/database.config';
 
-@Controller('api/v1/auth')
-export class AuthController {
+// @Controller('api/v1/auth')
+// export class AuthController {
 
-    private db: Db;
+//     private db: Db;
     
-    constructor() {}
+//     constructor() {}
     
-    async onModuleInit() {
-        this.db = await DatabaseService.connect();
-    }
+//     async onModuleInit() {
+//         this.db = await DatabaseService.connect();
+//     }
 
 
-    @Post('token')
-    generateToken(@Body('phoneNumber') phoneNumber: string) {
-      const JWT_SECRET = process.env.JWT_SECRET || 'YOUR_JWT_SECRET';
+//     @Post('token')
+//     generateToken(@Body('phoneNumber') phoneNumber: string) {
+//       const JWT_SECRET = process.env.JWT_SECRET || 'YOUR_JWT_SECRET';
 
-      if (!phoneNumber) {
-        return {
-            error: 'Phone number is required',
-            status: 400
-        }
-      }
+//       if (!phoneNumber) {
+//         return {
+//             error: 'Phone number is required',
+//             status: 400
+//         }
+//       }
   
-      const isPhoneNumberRegistered =  this.db.collection('users').findOne({ phoneNumber });
-      if (!isPhoneNumberRegistered) {
-        return {
-            error: 'Phone number is not registered',
-            status: 400
-        }
-      }
-      const token = jwt.sign({ phoneNumber }, JWT_SECRET, { expiresIn: '1h' });
-      return { token };
-    }
-  }
+//       const isPhoneNumberRegistered =  this.db.collection('users').findOne({ phoneNumber });
+//       if (!isPhoneNumberRegistered) {
+//         return {
+//             error: 'Phone number is not registered',
+//             status: 400
+//         }
+//       }
+//       const token = jwt.sign({ phoneNumber }, JWT_SECRET, { expiresIn: '1h' });
+//       return { token };
+//     }
+//   }
