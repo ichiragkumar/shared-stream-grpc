4492075b47ac7e3608659b9f6cf057420d30aa34
ichiragkumar <chiragroy3030@gmail.com>
2025-02-01 10:47:31 +0530

    remove gaurds

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+// import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController]
+  // controllers: [AuthController]
+  controllers: []
 })
 export class AuthModule {}
